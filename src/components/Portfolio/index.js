d6ff4c9fa0cec08c4f947dd7fbb68c239dc0c8ad
KarrenAD <nerrak87@gmail.com>
2020-09-20 23:28:05 -0600

    Updated portfolio.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,24 +7,15 @@ const Portfolio = () => {
     return (
         // 
         <div className="container h-50 bg-dark">
-            {/* <div className = "row mt-3">
-                <div className = "col-md-8">
-                    <div className = "page-header">
-                        <h1 className= "mt-4 text-white ">Portfolio</h1>
-                    </div>
-                </div>
-            </div> */}
-
             <div className = "row mt-3">
-
                 {/* CARD ONE */}
                 <div className="col-md-4">
                     <div className="mb-5 mt-3 card bg-secondary">
                         <div className="card-body">
-                            <div className="row">
+                            <div className="row justify-content-center">
                                 <h5 className="card-title text-center">Entertainment App!</h5>
                             </div>
-                        <div className="row">
+                        <div className="row justify-content-center">
                             <a className = "card-link text-warning" href="https://karrenad.github.io/media-api-group-project/">
                                 <img 
                                     src={bored} 
@@ -32,21 +23,20 @@ const Portfolio = () => {
                                 />
                             </a>
                         </div>
-                        <div className="mt-1 row">                            
+                        <div className="mt-1 row justify-content-center">                            
                             <p className = "text-warning"> Frontend app, featuring third party API calls! </p>
                         </div>
                         </div>
                     </div>                    
                 </div>
-
                 {/* CARD TWO */}
                 <div className="col-md-4">
                     <div className="mb-5 mt-3 card bg-secondary">
                         <div className="card-body">
-                            <div className="row">
+                            <div className="row justify-content-center">
                                 <h5 className="card-title text-center">BIER RUN!</h5>
                             </div>
-                        <div className="row">
+                        <div className="row justify-content-center">
                             <a className = "card-link text-warning" href="https://bier-run.herokuapp.com/">
                                 <img 
                                     src={bier} 
@@ -54,21 +44,20 @@ const Portfolio = () => {
                                 />
                             </a>
                         </div>
-                        <div className="mt-1 row">
+                        <div className="mt-1 row justify-content-center">
                             <p className = "text-warning"> THE Beer Delivery App for SLC </p>
                         </div>
                         </div>
                     </div>
                 </div>
-
                 {/* CARD THREE */}
                 <div className="col-md-4">
                     <div className="mb-5 mt-3 card bg-secondary">
                         <div className="card-body">
-                            <div className="row">
+                            <div className="row justify-content-center">
                                 <h5 className="card-title text-center">Weather App!</h5>
                             </div>
-                        <div className="row">
+                        <div className="row justify-content-center">
                             <a className = "card-link text-warning" href="https://karrenad.github.io/local-weather/">
                                 <img
                                     src={weather}
@@ -76,49 +65,38 @@ const Portfolio = () => {
                                 />
                             </a>
                         </div>
-                        <div className="mt-1 row">
+                        <div className="mt-1 row justify-content-center">
                             <p className = "text-warning"> Weather Forecaster, Featuring OpenWeather </p>
                         </div>
                         </div>
                     </div>
                 </div>
-
             {/* SMALLER ASSIGNMENTS */}
+            </div>
 
-              {/* <div className = "col-md-4">                
-              </div> */}
-              <div className="col-md-4">
+            <div class="row justify-content-center">
                 <div className="mb-5 mt-3 card bg-secondary" >
                   <div className="card-body">
-                    <div className="row">
+                    <div className="row justify-content-center">
                       <h5 className="card-title text-center">SMALL BUT NOTEWORTHY APPS!</h5>
                     </div>
-                    <div className="mt-1 row">
-                      <a className = "card-link text-warning" href="https://karrenad.github.io/day-planner/"> Day Planner Web App</a>
-                    </div>
-                    <div className="mt-1 row">
+                    <div className="mt-1 row justify-content-center">
                       <a className = "card-link text-warning" href="https://karrenad.github.io/password-generator/"> Password Generator</a>
                     </div>
-                    <div className="mt-1 row">
+                    <div className="mt-1 row justify-content-center">
+                      <a className = "card-link text-warning" href="https://karrenad.github.io/day-planner/"> Day Planner Web App</a>
+                    </div>
+                    <div className="mt-1 row justify-content-center">
                       <a className = "card-link text-warning" href="https://github.com/KarrenAD/readme-generator"> NodeJS README Generator</a>
                     </div>
-                    <div className="mt-1 row">
+                    <div className="mt-1 row justify-content-center">
                       <a className = "card-link text-warning" href="https://github.com/KarrenAD/mySQL-employee-database"> SQL Employee Database</a>
                     </div>
                   </div>
                 </div>
-              </div>
-
-
-
             </div>
 
-
-
-
-        </div>
-
-        
+        </div>        
     )
 }
 
