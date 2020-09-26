a1f3f3fd4d66b52a9b60ae202d48567af9effe3b
KarrenAD <nerrak87@gmail.com>
2020-09-26 03:48:09 -0600

    Updates

---
+++ precache-manifest.1fa4e5febafce191ba5a0408639c8bbf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38862c21a45344d25b706d2d323877a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "59ffa01f49ba64d01067",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e214c37d88feee8ab07",
+    "url": "/react-portfolio/static/js/2.babb38e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.babb38e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ffa01f49ba64d01067",
+    "url": "/react-portfolio/static/js/main.aecabbdd.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "21b43e2b7a2c545bdf516dc5f99919d0",
+    "url": "/react-portfolio/static/media/adkresume.21b43e2b.pdf"
+  },
+  {
+    "revision": "cba64367f3fcdef5ecdb1ead9954252e",
+    "url": "/react-portfolio/static/media/bier.cba64367.png"
+  },
+  {
+    "revision": "70f202dd92ab63bb929e9c2c97007339",
+    "url": "/react-portfolio/static/media/bored.70f202dd.png"
+  },
+  {
+    "revision": "8d30a8f4409a2614a58f16d22ef036e8",
+    "url": "/react-portfolio/static/media/hs2.8d30a8f4.png"
+  },
+  {
+    "revision": "4b72f01c5c07e9a77272980e2710271d",
+    "url": "/react-portfolio/static/media/navy.4b72f01c.png"
+  }
+]);
